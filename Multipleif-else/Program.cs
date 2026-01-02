14ec4caf197cbf02030fc01cd0327936ece5a0fb
chandu <dell@DESKTOP-GFR20G2>
2026-01-02 13:00:47 +0530

    I HAVE ADDED THE EXMAPLE

--- Multipleif-else/Program.cs
+++ Multipleif-else/Program.cs
@@ -10,6 +10,28 @@ namespace Multipleif_else
     {
         static void Main(string[] args)
         {
+            int score;
+            score = Convert.ToInt32(Console.ReadLine());
+            if (score >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
         }
     }
 }
